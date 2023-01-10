3951e553c9a71fb138486f1f1c89429d3522b7b8
Shi Tao Luo <sluo5991@gmail.com>
2023-01-11 07:32:07 +0900

    implement a limit of items showed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import Item from './components/Item';
 import data from './data';
+import { useState } from 'react';
 
 function App() {
-  const itemElement = data.map((item) => {
-    return <Item key={item.id} item={item} />;
-  });
+  // render more rows of data with a click more button
+  const [show, setShow] = useState(5);
 
-  return <div className='App'>{itemElement}</div>;
+  const showMore = () => {
+    setShow(show + 5);
+  };
+
+  const showLess = () => {
+    setShow(show - 5);
+  };
+
+  return (
+    <div className='App'>
+      {/* Show 5 items at first */}
+      {data.slice(0, show).map((item) => (
+        <Item item={item} />
+      ))}
+
+      {/* Show more/less items with a click button */}
+      {show <= 5 ? (
+        <button onClick={showMore}>Show More</button>
+      ) : (
+        <div>
+          <button onClick={showMore}>Show More</button>
+          <button onClick={showLess}>Show Less</button>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
