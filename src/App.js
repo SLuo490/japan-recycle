cbf5bbfbdfd684c9e619b653863389c8b2c6ff72
Shi Tao Luo <sluo5991@gmail.com>
2023-01-12 09:53:28 +0900

    add gap to grid

--- src/App.js
+++ src/App.js
@@ -5,27 +5,31 @@ import './index.css';
 
 function App() {
   // render more rows of data with a click more button
-  const [show, setShow] = useState(5);
+  const [show, setShow] = useState(4);
 
   const showMore = () => {
-    setShow(show + 5);
+    setShow(show + 4);
   };
 
   const showLess = () => {
-    setShow(show - 5);
+    setShow(show - 4);
   };
 
   return (
     <div className='App'>
       {/* Show 5 items at first */}
-      {data.slice(0, show).map((item) => (
-        <Item item={item} />
-      ))}
+      <div className='grid grid-cols-4 gap-5 m-20'>
+        {data.slice(0, show).map((item) => (
+          <Item item={item} />
+        ))}
+      </div>
       {/* Show more/less items with a click button */}
       {show <= 5 ? (
-        <button onClick={showMore}>Show More</button>
+        <div className='m-20'>
+          <button onClick={showMore}>Show More</button>
+        </div>
       ) : (
-        <div>
+        <div className='m-20'>
           <button onClick={showMore}>Show More</button>
           <button onClick={showLess}>Show Less</button>
         </div>
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,10 +1,10 @@
 export default function Item(props) {
-  console.log(props);
+  console.log(props.item.Category);
   return (
-    <div>
+    <div className='border w-30 h-36'>
       <div className='info'>
-        <h1>{props.item.ItemName} </h1>
-        <h3>{props.item.Category}</h3>
+        <h1>{props.item.ItemName}</h1>
+        <p>{props.item.Category}</p>
         <p>{props.item.Notes}</p>
       </div>
     </div>
