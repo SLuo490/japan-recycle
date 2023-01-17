5595c397e62bc69f813b026584c406a1ea4482e3
Shi Tao Luo <sluo5991@gmail.com>
2023-01-17 11:41:13 +0900

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Item from './components/Item';
+import Navbar from './components/Navbar';
 import data from './data';
 import { useState } from 'react';
 import './index.css';
@@ -17,21 +18,29 @@ function App() {
 
   return (
     <div className='App'>
+      <Navbar />
       {/* Show 5 items at first */}
-      <div className='grid grid-cols-4 gap-5 m-20'>
+      <div className='grid grid-cols-4 gap-5 mx-20 mt-20'>
         {data.slice(0, show).map((item) => (
           <Item item={item} />
         ))}
       </div>
       {/* Show more/less items with a click button */}
       {show <= 5 ? (
-        <div className='m-20'>
+        <div className='mx-20 mt-5'>
           <button onClick={showMore}>Show More</button>
         </div>
       ) : (
-        <div className='m-20'>
-          <button onClick={showMore}>Show More</button>
-          <button onClick={showLess}>Show Less</button>
+        <div className='mx-20 mt-5'>
+          <button
+            className='mr-2 border-b-4 border-indigo-500'
+            onClick={showMore}
+          >
+            Show More
+          </button>
+          <button className='border-b-4 border-indigo-500' onClick={showLess}>
+            Show Less
+          </button>
         </div>
       )}
     </div>
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,12 +1,10 @@
 export default function Item(props) {
   console.log(props.item.Category);
   return (
-    <div className='border w-30 h-36'>
-      <div className='info'>
-        <h1>{props.item.ItemName}</h1>
-        <p>{props.item.Category}</p>
-        <p>{props.item.Notes}</p>
-      </div>
+    <div className='border w-30 h-36 flex flex-col items-center justify-center'>
+      <h1>{props.item.ItemName}</h1>
+      <p>{props.item.Category}</p>
+      <p>{props.item.Notes}</p>
     </div>
   );
 }
