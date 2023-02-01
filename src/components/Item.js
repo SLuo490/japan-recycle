af984a6e182c06eaf4f58f5e72cc401f3172dcdb
Shi Tao Luo <sluo5991@gmail.com>
2023-02-01 14:39:32 -0500

    add color to item depending on category

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,7 +1,33 @@
 export default function Item(props) {
-  console.log(props.item.Category);
+  var category = props.item.Category;
+
+  // set color of the background depending on the category of the item
+  const color = () => {
+    if (category === 'Recyclable') {
+      return 'bg-green-300';
+    } else if (category === 'Non-burnable') {
+      return 'bg-lime-200';
+    } else if (category === 'Burnable') {
+      return 'bg-red-300';
+    } else if (
+      category === 'Oversized Non-burnable' ||
+      category === 'Oversized Burnable'
+    ) {
+      return 'bg-pink-300';
+    } else if (category === "We Don't Collect") {
+      return 'bg-orange-300';
+    } else if (category === 'Plastic') {
+      return 'bg-gray-300';
+    } else if (category === 'Hazardous waste') {
+      return 'bg-gray-400';
+    }
+  };
+
   return (
-    <div className='border w-30 h-36 flex flex-col items-center justify-center'>
+    // set the background color of the item card
+    <div
+      className={`border w-30 h-36 flex flex-col items-center justify-center ${color()}`}
+    >
       <h1>{props.item.ItemName}</h1>
       <p>{props.item.Category}</p>
       <p>{props.item.Notes}</p>
